cef106288c8c06f89316c4995b3cf4290ca44044
Yassine EL-MANSOURY <yassineelmansoury1000@gmail.com>
2024-12-16 16:55:31 +0100

    app.js

--- Module_Emojins/app.js
+++ Module_Emojins/app.js
@@ -5,12 +5,5 @@ console.log(emojinsdata.findimojin("happy"));
 // console.log(imojinsdata.findimojin("sad"));
 // console.log(imojinsdata.findimojin("dog"));
 // console.log(imojinsdata.findimojin("surprised"));
-// console.log(imojinsdata.findimojin("hero"));
+// console.log(imojinsdata.findimojin("superman")); 
 
-
-
-
-// this is a small and simple module that is used for getting 
-//emojins, you can use it by writting a value in function findimojin 
-//and it return your the value that you search for.
- 
